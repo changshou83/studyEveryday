8b58120d8195cb1281123fe2ed36b8bcb8348473
changshou83 <1369548156@qq.com>
2022-04-18 19:51:31 +0800

    ✨ feat(ct/tt): add template

---
+++ codewars tools/testTools/template.mjs
@@ -0,0 +1,13 @@
+import { describe, it, expect, showTestsResults } from './cstest.mjs';
+const log = console.log;
+
+describe('Basic Tests', () => {
+  it('Pass Tests', () => {
+    expect().toBe();
+  });
+  it('Failed Tests', () => {
+    expect().toBe();
+  });
+});
+
+showTestsResults();
