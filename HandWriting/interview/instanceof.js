810a458d02de43a49573f6c214835cbb821fdeb7
changshou83 <1369548156@qq.com>
2022-07-27 21:06:26 +0800

    fix(instanceof): syntax wrong

--- HandWriting/interview/instanceof.js
+++ HandWriting/interview/instanceof.js
@@ -1,9 +1,9 @@
 const MyInstanceof = (child, parent) => {
-  const childProto = Object.getPrototypeOf(child);
+  let childProto = Object.getPrototypeOf(child)
   while (true) {
     if (childProto === null || childProto === undefined) {
-      return false;
-    } else if (childProto === parent) return true;
-    childProto = Object.getPrototypeOf(childProto);
+      return false
+    } else if (childProto === parent.prototype) return true
+    childProto = Object.getPrototypeOf(childProto)
   }
-};
+}
